9595d4e7651a3eb4b2b613211f3b3650f6e2711e
praiseOjay <p.o.ojerinola@edu.salford.ac.uk>
2025-11-24 11:37:15 +0000

    top rank page completed

--- src/main.py
+++ src/main.py
@@ -3,12 +3,18 @@ import streamlit as st
 
 
 def main():
-    df = pd.read_csv("./pokemon_v2.csv")
+    # df = pd.read_csv("./pokemon_v2.csv")
     # example:
     # home_page = st.page("path/to/home_page.py", title="Home Page")
     # list pages here
     # ...
-
+    top_ranks = st.page(
+        "../src/pages/top_rank.py",
+        title="Top Ranks",
+        icon="star",
+    )
+    pages = st.navigation([top_ranks])
+    pages.run()
     # pages = st.navigation([list, pages, here])
     # pages.run()
 
